5ce6cc434d54354b4bdf8c6a20da7418ee4c7dde
mgrouch <mgrushinskiy@gmail.com>
2021-09-17 22:40:01 -0400

    BBN customizations

--- run_pat.sh
+++ run_pat.sh
@@ -40,13 +40,6 @@ amixer -c ${CARDNUM} set PCM ${OUTPUT_VOL} > /dev/null
 ardopc > /tmp/ardopc.log &
 sleep 1
 
-# IC-705 provides GPS on ttyACM1. TODO: This will need to be made dynamic later for other devices.
-#echo 'Starting gpsd...'
-#sudo service gpsd stop
-#sudo killall gpsd
-#gpsd /dev/ttyACM1
-
-echo
 uimode=1
 if [ "$#" -lt "1" ]; then
   echo 'Start Pat with web GUI or interactive text mode?'
